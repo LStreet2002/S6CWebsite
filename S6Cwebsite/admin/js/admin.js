87a69020ea81a0478296f70f81f4c8396c5949bc
Lewis Street <lstreet@salisbury6c.ac.uk>
2020-10-14 14:17:19 +0100

    login done

---
+++ S6Cwebsite/admin/js/admin.js
@@ -0,0 +1,17 @@
+(function () {
+    const loginHolder = document.querySelector("#login-holder");
+    loginHolder.addEventListener("submit", (e) => {
+        e.preventDefault(); //enter button
+        console.log("did")
+
+        const email = loginHolder["login-email"].value;
+        const password = loginHolder["login-password"].value;
+
+        // firebase login
+        auth.signInWithEmailAndPassword(email, password).then((cred) => {
+            document.querySelector("#login-email").style.border = "solid 2px green";
+            window.location.replace("menu.html")
+        }).catch(error => document.querySelector("#login-password").style.border = "solid 1px red"
+            , document.querySelector("#login-email").style.border = "solid 1px red"), console.log(email), console.log(password);
+    })
+})();
--- S6Cwebsite/admin/js/firebase.js
+++ S6Cwebsite/admin/js/firebase.js
@@ -13,5 +13,4 @@ var firebaseConfig = {
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
-firebase.analytics();
 auth = firebase.auth();
