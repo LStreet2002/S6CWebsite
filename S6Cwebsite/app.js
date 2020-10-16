cf7b8aa5ebbf2956200b5c9f9806d694d8996a30
new-byt <55684787+new-byt@users.noreply.github.com>
2020-10-16 11:31:32 +0100

    Fixed?

--- S6Cwebsite/app.js
+++ S6Cwebsite/app.js
@@ -2,9 +2,8 @@ const express = require("express"),
   bodyParser = require("body-parser");
 const app = express();
 const port = 3000;
-var fs = require("fs");
-app.use(express.static("admin"));
-app.use(bodyParser.json());
+
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/adminLogin.html");
@@ -19,20 +18,7 @@ app.get("/edit-home", (req, res) => {
   res.sendFile(__dirname + "/editHome.html");
 });
 app.get("/edit-info", (req, res) => {
-  var fileList = [];
-  fs.readdirSync("infoTabs/").forEach((file) => {
-    fileList.push(file);
-  });
-  console.log(fileList);
   res.sendFile(__dirname + "/editInfo.html");
-});
-
-app.post("/", (req, res) => {
-  console.log(req.body);
-  fs.writeFile("infoTabs/" + req.body["file"] + ".html", "", function (err) {
-    if (err) throw err;
-    console.log("File is created successfully.");
-  });
 });
 
 app.get("/edit-study", (req, res) => {
