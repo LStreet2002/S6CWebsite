b5f1db11f349c11664b9a50200aab0eb4c15c9d5
new-byt <55684787+new-byt@users.noreply.github.com>
2020-10-20 11:21:11 +0100

    yes

--- S6Cwebsite/app.js
+++ S6Cwebsite/app.js
@@ -1,30 +1,31 @@
 const express = require("express"),
   bodyParser = require("body-parser");
+const exphbs = require('express-handlebars');
 const app = express();
 const port = 3000;
+
+//app.set('view engine', 'hbs')
 
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/adminLogin.html");
+  res.sendFile(__dirname + '/adminLogin.html');
 });
 app.get("/menu", (req, res) => {
-  res.sendFile(__dirname + "/menu.html");
-});
-app.get("/edit-courses", (req, res) => {
-  res.sendFile(__dirname + "/editCourses.html");
-});
-app.get("/edit-home", (req, res) => {
-  res.sendFile(__dirname + "/editHome.html");
+  res.sendFile(__dirname + '/menu.html');
 });
 app.get("/edit-info", (req, res) => {
-  res.sendFile(__dirname + "/editInfo.html");
+  res.sendFile(__dirname + '/editInfo.html');
 });
-
+app.get("/edit-home", (req, res) => {
+  res.sendFile(__dirname + '/editHome.html');
+});
 app.get("/edit-study", (req, res) => {
-  res.sendFile(__dirname + "/editStudy.html");
+  res.sendFile(__dirname + '/editStudy.html');
 });
-
+app.get("/edit-courses", (req, res) => {
+  res.sendFile(__dirname + '/editCourses.html');
+});
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
