c906a9a27f1cce97f4ad578b67f7308c8184f38e
Lewis Street <lstreet@salisbury6c.ac.uk>
2020-10-20 14:22:29 +0100

    blog admin

--- S6Cwebsite/public/admin/js/PB.js
+++ S6Cwebsite/public/admin/js/PB.js
@@ -17,24 +17,25 @@ async function old() {
     for (i = 0; i < decay.length; i++) {
         var hold = document.createElement("div")
         hold.classList.add("post")
+        hold.id = decay[i].title
         var time = document.createElement("h2")
         time.innerText = decay[i].date
         var edit = document.createElement("span")
         edit.classList.add("edit", "material-icons")
         edit.innerText = "edit"
-        edit.setAttribute("onclick", "edit()")
+        edit.setAttribute("onclick", "edit(this)")
         var del = document.createElement("span")
         del.classList.add("delete", "material-icons")
         del.innerText = "delete"
-        del.setAttribute("onclick", "delet()")
+        del.setAttribute("onclick", "delet(this)")
         var title = document.createElement("input")
         title.classList.add("postit")
         title.setAttribute("type", "text")
-        title.setAttribute("value", decay[i].title)
-        var desc = document.createElement("input")
+        title.setAttribute("value", decay[i].title2)
+        var desc = document.createElement("textarea")
         desc.classList.add("postesc")
         desc.setAttribute("type", "text")
-        desc.setAttribute("value", decay[i].desc)
+        desc.innerText = decay[i].desc
         var hr = document.createElement("hr")
 
         hold.appendChild(time)
@@ -45,5 +46,77 @@ async function old() {
         hold.appendChild(hr)
 
         document.querySelector("#postlist").appendChild(hold)
+    }
+}
+
+function delet(e) {
+    db.collection("Firth").doc(e.parentNode.id).delete()
+    e.parentNode.style.display = "none"
+}
+function edit(e) {
+    if (e.parentNode.querySelector(".postit").style.display == "none") {
+        e.parentNode.querySelector(".postit").style.display = "block"
+        e.parentNode.querySelector(".postesc").style.display = "block"
+    }
+    else {
+        e.parentNode.querySelector(".postit").style.display = "none"
+        e.parentNode.querySelector(".postesc").style.display = "none"
+    }
+}
+
+async function make() {
+    var tut = document.querySelector("#addPost")
+    var dec = document.querySelector("#addContent")
+    var det = document.querySelector("#addDate")
+    db.collection("Firth").doc(document.querySelector("#addPost").value).set({
+        title: tut.value,
+        desc: dec.value,
+        title2: tut.value,
+        date: det.value
+    })
+
+    var hold = document.createElement("div")
+    hold.classList.add("post")
+    hold.id = tut.value
+    var time = document.createElement("h2")
+    time.innerText = det.value
+    var edit = document.createElement("span")
+    edit.classList.add("edit", "material-icons")
+    edit.innerText = "edit"
+    edit.setAttribute("onclick", "edit(this)")
+    var del = document.createElement("span")
+    del.classList.add("delete", "material-icons")
+    del.innerText = "delete"
+    del.setAttribute("onclick", "delet(this)")
+    var title = document.createElement("input")
+    title.classList.add("postit")
+    title.setAttribute("type", "text")
+    title.setAttribute("value", tut.value)
+    var desc = document.createElement("textarea")
+    desc.classList.add("postesc")
+    desc.setAttribute("type", "text")
+    desc.innerText = dec.value
+    var hr = document.createElement("hr")
+
+    hold.appendChild(time)
+    hold.appendChild(edit)
+    hold.appendChild(del)
+    hold.appendChild(title)
+    hold.appendChild(desc)
+    hold.appendChild(hr)
+
+    document.querySelector("#postlist").appendChild(hold)
+    tut.value = ""
+    dec.value = ""
+    det.value = ""
+
+}
+async function save() {
+    var blogs = document.getElementsByClassName("post")
+    for (i = 0; i < blogs.length; i++) {
+        db.collection("Firth").doc(blogs[i].id).update({
+            title2: blogs[i].querySelector(".postit").value,
+            desc: blogs[i].querySelector(".postesc").value,
+        })
     }
 }
