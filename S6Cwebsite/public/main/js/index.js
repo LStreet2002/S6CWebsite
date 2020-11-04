2ff11ba78052e523e6af2d12f92b7c1c420ab375
Lewis Street <lstreet@salisbury6c.ac.uk>
2020-11-04 12:50:54 +0000

    get to info

--- S6Cwebsite/public/main/js/index.js
+++ S6Cwebsite/public/main/js/index.js
@@ -4,3 +4,7 @@ const navbarLinks = document.getElementsByClassName('navbar-links')[0]
 toggleButton.addEventListener('click', () => {
     navbarLinks.classList.toggle('active')
 })
+function chang(e) {
+    document.querySelector("#something").value = "bursary"
+    document.querySelector("#submit").click()
+}
