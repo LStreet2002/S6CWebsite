526c2fee7248ba70063fe08299190e7592b9ce25
Lance <55697247+Lance-png@users.noreply.github.com>
2020-11-10 10:01:07 +0000

    Finished page layout, ready to add JS and later style

---
+++ S6Cwebsite/public/admin/js/courses.js
@@ -0,0 +1,6 @@
+var newt = [];
+var pageNameInput = document.getElementsByClassName("pageNameInput")[0];
+var headerInput = document.getElementsByClassName("headerInput")[0];
+var contentInput = document.getElementsByClassName("contentInput")[0];
+var updateButton = document.getElementsByClassName("updateButton")[0];
+var page = document.getElementsByTagName("title")[0].className;
