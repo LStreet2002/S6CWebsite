81458bff030b6dc4eef28df743cf643780be6659
new-byt <55684787+new-byt@users.noreply.github.com>
2020-11-14 15:45:35 +0000

    Update admin.js

--- public/admin/js/admin.js
+++ public/admin/js/admin.js
@@ -2,32 +2,27 @@ firebase
   .auth()
   .setPersistence(firebase.auth.Auth.Persistence.SESSION)
   .then(function () {
-    (function () {
-      const loginHolder = document.querySelector("#login-holder");
-      loginHolder.addEventListener("submit", (e) => {
-        e.preventDefault(); //enter button
-        console.log("did");
-
-        const email = loginHolder["login-email"].value;
-        const password = loginHolder["login-password"].value;
-
-        // firebase login
-        auth
-          .signInWithEmailAndPassword(email, password)
-          .then((cred) => {
-            document.querySelector("#login-email").style.border =
-              "solid 1px green";
-            window.location.replace("menu");
-          })
-          .catch(
-            (error) =>
-              (document.querySelector("#login-password").style.border =
-                "solid 1px red"),
-            (document.querySelector("#login-email").style.border =
-              "solid 1px red")
-          ),
-          console.log(email),
-          console.log(password);
-      });
-    })();
+    const loginHolder = document.querySelector("#login-holder");
+    loginHolder.addEventListener("submit", (e) => {
+      e.preventDefault(); //enter button
+      console.log("did");
+      const email = loginHolder["login-email"].value;
+      const password = loginHolder["login-password"].value;
+      firebase
+        .auth()
+        .signInWithEmailAndPassword(email, password)
+        .then(() => {
+          document.querySelector("#login-email").style.border =
+            "solid 1px green";
+          window.location.replace("menu");
+        })
+        .catch(function (error) {
+          // Handle Errors here.
+          var errorCode = error.code;
+          var errorMessage = error.message;
+          document.querySelector("#login-password").style.border =
+            "solid 1px red";
+          document.querySelector("#login-email").style.border = "solid 1px red";
+        });
+    });
   });
