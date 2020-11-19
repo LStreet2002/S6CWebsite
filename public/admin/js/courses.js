46f7c42525196cf043adafecc291184955a32101
Lewis Street <lstreet@salisbury6c.ac.uk>
2020-11-19 11:59:08 +0000

    examples

--- public/admin/js/courses.js
+++ public/admin/js/courses.js
@@ -7,3 +7,19 @@ var assessmentInput = document.getElementsByClassName("examInput")[0];
 var descriptionInput = document.getElementsByClassName("examInput")[0];
 var saveButton = document.getElementsByClassName("saveButton")[0];
 var page = document.getElementsByTagName("title")[0].className;
+
+/* getting files  async function () {
+    await db.collection("collection name")
+        .get()
+        .then(async function (querySnapshot) {
+            querySnapshot.forEach(function (doc) {
+                // doc.data() is never undefined for query doc snapshots
+                carouse.push(doc.data())
+
+            })
+        })
+    }*/
+
+/* saving files db.collection("collection").doc("document").update({
+    name: document.querySelector("#editnews").value
+})*/
