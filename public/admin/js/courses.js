d7268d500430d3d3813fb6af7ade14668171875c
Lance <55697247+Lance-png@users.noreply.github.com>
2020-11-19 11:55:50 +0000

    Correctly Links

--- public/admin/js/courses.js
+++ public/admin/js/courses.js
@@ -7,3 +7,12 @@ var assessmentInput = document.getElementsByClassName("examInput")[0];
 var descriptionInput = document.getElementsByClassName("examInput")[0];
 var saveButton = document.getElementsByClassName("saveButton")[0];
 var page = document.getElementsByTagName("title")[0].className;
+
+async function addCourse() {
+    var input = document.getElementsByClassName("addCourseInput")[0].value;
+    var titl = input.toLowerCase();
+    await db.collection(page).doc(titl).set({
+        pageName: input,
+    });
+    location.reload();
+}
