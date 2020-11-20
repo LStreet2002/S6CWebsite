4757c09086efca72397b6fe3727770d3e8389e69
Lewis Street <lstreet@salisbury6c.ac.uk>
2020-11-20 13:25:13 +0000

    nav mobile

--- public/main/js/index.js
+++ public/main/js/index.js
@@ -82,7 +82,7 @@ async function newce() {
     .then(async function (querySnapshot) {
       querySnapshot.forEach(function (doc) {
         // doc.data() is never undefined for query doc snapshots
-        document.querySelector(".notice").innerHTML += doc.data().name;
+        document.querySelector(".notice").innerHTML = doc.data().name;
       });
     });
 }
