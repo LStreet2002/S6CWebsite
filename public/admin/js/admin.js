be91bb0e3d3feca16f0e8dee79f1cb7c6d87ed76
Lewis Street <lstreet@salisbury6c.ac.uk>
2020-12-05 18:48:11 +0000

    medias

--- public/admin/js/admin.js
+++ public/admin/js/admin.js
@@ -1,47 +1,52 @@
-firebase
-  .auth()
-  .setPersistence(firebase.auth.Auth.Persistence.SESSION)
-  .then(function () {
-    const loginHolder = document.querySelector("#login-holder");
-    loginHolder.addEventListener("submit", (e) => {
-      e.preventDefault(); //enter button
-      console.log("did");
-      const email = loginHolder["login-email"].value;
-      const password = loginHolder["login-password"].value;
-      firebase
-        .auth()
-        .signInWithEmailAndPassword(email, password)
-        .then(() => {
-          return firebase
-            .auth()
-            .currentUser.getIdToken()
-            .then((idToken) => {
-              postToBack(idToken);
-            });
-        })
-        .catch(function (error) {
-          // Handle Errors here.
-          var errorCode = error.code;
-          var errorMessage = error.message;
-          document.querySelector("#Login").style.backgroundColor =
-            "#b12e2e";
-          setTimeout(() => {
+if (screen.width < 600) {
+  window.location.replace("index.html");
+}
+else {
+  firebase
+    .auth()
+    .setPersistence(firebase.auth.Auth.Persistence.SESSION)
+    .then(function () {
+      const loginHolder = document.querySelector("#login-holder");
+      loginHolder.addEventListener("submit", (e) => {
+        e.preventDefault(); //enter button
+        console.log("did");
+        const email = loginHolder["login-email"].value;
+        const password = loginHolder["login-password"].value;
+        firebase
+          .auth()
+          .signInWithEmailAndPassword(email, password)
+          .then(() => {
+            return firebase
+              .auth()
+              .currentUser.getIdToken()
+              .then((idToken) => {
+                postToBack(idToken);
+              });
+          })
+          .catch(function (error) {
+            // Handle Errors here.
+            var errorCode = error.code;
+            var errorMessage = error.message;
             document.querySelector("#Login").style.backgroundColor =
-              "#662483";
-          }, 2500);
-        });
+              "#b12e2e";
+            setTimeout(() => {
+              document.querySelector("#Login").style.backgroundColor =
+                "#662483";
+            }, 2500);
+          });
+      });
     });
-  });
-function postToBack(idToken) {
-  console.log(idToken);
-  fetch("/adminLogin", {
-    headers: {
-      "Content-Type": "application/json",
-    },
-    method: "POST",
-    body: JSON.stringify({ idToken: idToken }),
-  }).then((res) => {
-    console.log("Request complete! response:", res);
-    window.location.replace("menu");
-  });
+  function postToBack(idToken) {
+    console.log(idToken);
+    fetch("/adminLogin", {
+      headers: {
+        "Content-Type": "application/json",
+      },
+      method: "POST",
+      body: JSON.stringify({ idToken: idToken }),
+    }).then((res) => {
+      console.log("Request complete! response:", res);
+      window.location.replace("menu");
+    });
+  }
 }
