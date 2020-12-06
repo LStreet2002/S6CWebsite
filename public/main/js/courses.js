0cdaeaa8b3c060b9627595c5efddd1a47f9d5087
Lewis Street <lstreet@salisbury6c.ac.uk>
2020-12-06 21:41:25 +0000

    cwrse

--- public/main/js/courses.js
+++ public/main/js/courses.js
@@ -1,6 +1,7 @@
 window.addEventListener("DOMContentLoaded", function () {
 	getDatabase();
 	getPages();
+	listadd()
 });
 async function getDatabase() {
 	const ref = db.collection("courses");
@@ -34,3 +35,50 @@ async function getDatabase() {
 		document.getElementById("courses").appendChild(a);
 	});
 }
+async function listadd() {
+	const ref = db.collection("courses");
+	const snapshot = await ref.get();
+	snapshot.forEach((doc) => {
+		//Course title
+		var courseId = document.createElement("a");
+		courseId.innerHTML = doc.data().pageName + "<br>";
+		courseId.href = "/course?page=" + doc.data().pageName.toLowerCase();
+		//Add course div to page
+		document.getElementById("searchblob").appendChild(courseId);
+	});
+}
+var searchBox = document.querySelector(".search");
+events = ["keyup", "focus", "blur"];
+events.forEach((event) => {
+	switch (event) {
+		case "keyup":
+			//SEARCH FUNCTION
+			searchBox.addEventListener("keyup", function () {
+				var input = document.querySelector(".search");
+				var filter = input.value.toUpperCase();
+				var ul = document.getElementById("searchblob");
+				var li = ul.getElementsByTagName("a");
+
+				for (i = 0; i < li.length; i++) {
+					//LOOPS THROUGH ALL TEXT VALUES
+					var txtValue = li[i].innerText;
+					if (txtValue.toUpperCase().indexOf(filter) > -1) {
+						li[i].style.display = "";
+					} else {
+						li[i].style.display = "none";
+					}
+				}
+			});
+		case "blur":
+			searchBox.addEventListener("blur", function () {
+				setTimeout(() => {
+					document.querySelector("#searchblob").style.display = "none";
+				}, 600);
+				searchBox.value = "";
+			});
+		case "focus":
+			searchBox.addEventListener("focus", function () {
+				document.querySelector("#searchblob").style.display = "block";
+			});
+	}
+});
